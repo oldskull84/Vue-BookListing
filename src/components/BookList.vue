7e6ef16a97a56dc4fe31a930836dd9bdc48740f8
OldSkuLL <vitaitamas@gmail.com>
2022-05-15 12:53:59 +0200

    Looping Through a List of Books

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -3,6 +3,9 @@
     <h1>
         {{ title }}
     </h1>
+    <ul>
+        <li v-for="book in books">{{ book.title }}:{{ book.author }}</li>
+    </ul>
 </div>
     
 </template>
@@ -10,10 +13,13 @@
 <script>
 export default {
     name: 'BookList',
-    data() {
-        return {
-            title: 'All Books'
-        }
+    data: {
+        title: 'All Books',
+        books: [
+            { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' },
+            { title: 'American Gods', author: 'Neil Gaiman' },
+            { title: 'Amusing Ourselves to Death', author: 'Neil Postman' },
+        ]
     }
 }
 </script>
@@ -21,5 +27,15 @@ export default {
 <style scoped>
 h1, h2 {
     font-weight: normal;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: block;
+    margin: 0 10px;
 }
 </style>
